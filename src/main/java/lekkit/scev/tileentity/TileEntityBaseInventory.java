cf821487c0d078b7f837ebf6ae4de62b2940da52
LekKit <50500857+LekKit@users.noreply.github.com>
2024-07-07 18:40:50 +0300

    tileentity: Fix GUI closing on broken container

--- src/main/java/lekkit/scev/tileentity/TileEntityBaseInventory.java
+++ src/main/java/lekkit/scev/tileentity/TileEntityBaseInventory.java
@@ -78,8 +78,10 @@ public class TileEntityBaseInventory extends TileEntityBase implements IInventor
     }
 
     @Override
-    public boolean isUseableByPlayer(EntityPlayer entityplayer) {
-        return true;
+    public boolean isUseableByPlayer(EntityPlayer player) {
+        // Is the TileEntity still alive and reachable by the player?
+        return this.worldObj.getTileEntity(this.xCoord, this.yCoord, this.zCoord) == this
+            && player.getDistanceSq((double)this.xCoord + 0.5D, (double)this.yCoord + 0.5D, (double)this.zCoord + 0.5D) <= 64.0D;
     }
 
     @Override
